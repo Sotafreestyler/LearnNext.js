54e74cdb2ade09d5ac8b72a57a6a8fa6e39d3e6a
ItoSota <>
2021-07-22 17:57:34 +0900

    Next.js学習用

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     <header>
       <nav>
         <Link href="/">
-          <a>Home</a>
+          <a>ホーム</a>
         </Link>{' '}
         |{' '}
         <Link href="/about">
@@ -25,7 +25,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         </Link>{' '}
         |{' '}
         <Link href="/users">
-          <a>Users List</a>
+          <a>ユーザーリスト</a>
         </Link>{' '}
         | <a href="/api/users">Users API</a>
       </nav>
